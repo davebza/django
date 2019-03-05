216a7d0bb78372e300edcf21a99cf5708c899fc0
davebza <daveb.za@gmail.com>
2019-03-05 09:49:01 +0800

    Added list views as a class-based method

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,15 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+
 
 # Create your views here.
-
-#old view:
-# def post_list(request):
-#     posts = Post.published.all()
-#     return render(request,
-#                   'blog/post/list.html',
-#                   {'posts': posts})
 
 def post_list(request):
     object_list = Post.published.all()
@@ -23,7 +18,7 @@ def post_list(request):
     except EmptyPage:
         #if page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list/html',
+    return render(request, 'blog/post/list.html',
                   {'page': page,
                    'posts': posts})
 
@@ -38,5 +33,10 @@ def post_detail(request, year, month, day, post):
                   'blog/post/detail.html',
                   {'post': post})
 
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
