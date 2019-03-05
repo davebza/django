e37bce0f4524276ebeb28ea6db828a9b8547434b
davebza <daveb.za@gmail.com>
2019-03-05 21:28:58 +0800

    Before drastic change on views.py, overwriting my version of the function post_share(request, post_id)

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -44,6 +44,7 @@ class PostListView(ListView):
 def post_share(request, post_id):
     #Retrieve post by id:
     post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
 
     if request.method == 'POST':
         #Form was submitted:
@@ -53,7 +54,7 @@ def post_share(request, post_id):
             cd = form.cleaned_data
             #Send email:
             post.url = request.build_absolute_uri(post.get_absolute_url())
-            subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
+            subject = '{} ({}) recommends you to read "{}"'.format(cd['name'], cd['email'], post.title)
             message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post.url, cd['name'], cd['comments'])
             send_mail(subject, message, 'admin@myblog.com', [cd['to']])
             sent = True
