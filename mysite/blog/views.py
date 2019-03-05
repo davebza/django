d2944d4a811835ae58f3a0445465fb7da855e80d
davebza <daveb.za@gmail.com>
2019-03-05 13:46:53 +0800

    Added functional email to my gmail test account. The details have been removed and the lines are commented in this commit. Subsequent commits will omit this file for security, until it is changed again, and then the details will be redacted again.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,6 +3,7 @@ from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 
 # Create your views here.
@@ -50,10 +51,17 @@ def post_share(request, post_id):
         if form.is_valid():
             #Form fields pass validation
             cd = form.cleaned_data
-            #...send email
+            #Send email:
+            post.url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post.url, cd['name'], cd['comments'])
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            sent = True
+
     else:
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post':post,
-                                                    'form': form})
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
 
 
